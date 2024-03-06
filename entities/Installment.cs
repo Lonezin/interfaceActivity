84b0033f67ea3394790eb0b3d44f6374daa2e2cd
Vinicius Morais <vinidev.morais@gmail.com>
2024-03-06 12:14:55 -0300

    Created Class Contract and Installment

---
+++ entities/Installment.cs
@@ -0,0 +1,20 @@
+using System.Globalization;
+
+namespace PaymentCompany
+{
+    public class Installment
+    {
+    public double Amount { get; set; }
+    public DateTime DueDate  { get; set; }
+
+    public Installment(double amount, DateTime dueDate)
+    {
+        Amount = amount;
+        DueDate = dueDate;
+    }
+        public override string ToString()
+        {
+            return $"{DueDate.ToString("MM/dd/yyyy")} - {Amount.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
